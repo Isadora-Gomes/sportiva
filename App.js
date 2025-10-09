f5e0c7b8e0bc9cd25191261cc3d6656a2bc44b37
Ana-Livia-Lopes <ana.livia.santos.lopes@gmail.com>
2025-10-09 15:21:00 -0300

    Produtos front

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import "react-native-gesture-handler";
 import { View, StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
@@ -14,18 +15,20 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <StatusBar barStyle="light-content" backgroundColor="rgb(19, 19, 24)" />
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Entrar">
+    <SafeAreaProvider>
+      <View style={{ flex: 1 }}>
+        <StatusBar barStyle="light-content" backgroundColor="rgb(19, 19, 24)" />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Entrar">
           <Stack.Screen name="Pesquisa" component={Pesquisa} />
           <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false, drawerItemStyle: { display: 'none' } }} />
           <Stack.Screen name="Entrar" component={Entrar} options={{ headerShown: false, drawerItemStyle: { display: 'none' } }} />
           <Stack.Screen name="Produto" component={Produto} options={{ headerShown: false, drawerItemStyle: { display: 'none' } }} />
           <Stack.Screen name="Inicio" component={Inicio} options={{ title: "Inicio" }} />
           <Stack.Screen name="Carrinho" component={Carrinho} options={{ title: "Carrinho" }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    </SafeAreaProvider>
   );
 }
--- src/screens/Produto.js
+++ src/screens/Produto.js
@@ -1,16 +1,30 @@
 import React from "react";
 import {View, Text, Image, ImageBackground, ScrollView, TouchableOpacity, StyleSheet} from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function App() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <ScrollView style={estilos.tela} contentContainerStyle={estilos.conteudoTela}>
-      <ImageBackground
-        source={{ uri: "" }}
-        style={estilos.hero}
+    <SafeAreaView
+        style={{ flex: 1, backgroundColor: '#0f0f10' }}
       >
-        <Text style={estilos.logo}>SPORTIVA</Text>
-        <Text style={estilos.tituloHero}>
-          Encontre aqui os <Text style={estilos.destaque}>melhores</Text> produtos de esporte
+      <ScrollView
+        style={estilos.tela}
+        contentContainerStyle={[estilos.conteudoTela]}
+      >
+      <ImageBackground
+        source={require('../../assets/img/fundo_produtos.png')}
+        style={estilos.Inicio}
+        imageStyle={{ opacity: 0.5 }}
+      >
+        <Image
+            source={require('../../assets/img/logoSlim.png')}
+            style={estilos.logo}
+          />
+        <Text style={estilos.tituloInicio}>
+          Encontre aqui os {'\n'}<Text style={estilos.destaque}>melhores</Text> produtos de {'\n'}esporte
         </Text>
       </ImageBackground>
 
@@ -21,8 +35,8 @@ export default function App() {
       <View style={estilos.itemProduto}>
         <View style={estilos.colPreco}>
           <Image
-            source={{ uri: "" }}
-            style={estilos.thumb}
+            source={require('../../assets/img/produto1.png')}
+            style={estilos.produto}
           />
           <Text style={estilos.preco}>R$ 500,00</Text>
         </View>
@@ -30,13 +44,13 @@ export default function App() {
           <View style={estilos.topoItem}>
             <Text style={estilos.nome}>Tênis Vulcan</Text>
             <View style={estilos.notaBox}>
-              <Text style={estilos.notaTexto}>4.5 ★</Text>
+              <Text style={estilos.notaTexto}>4.5 <Icon name="star" size={12} color="#ffd455" /></Text>
             </View>
           </View>
           <Text style={estilos.descricao}>
             Estilo e desempenho em cada passo. O Tênis Vulcan combina design moderno
-            com conforto superior — ideal para quem busca presença e segurança no
-            dia a dia ou na prática esportiva.
+            com conforto — ideal para quem busca presença e segurança no
+            dia a dia.
           </Text>
         </View>
       </View>
@@ -44,8 +58,8 @@ export default function App() {
       <View style={estilos.itemProduto}>
         <View style={estilos.colPreco}>
           <Image
-            source={{ uri: "" }}
-            style={estilos.thumb}
+            source={require('../../assets/img/produto2.png')}
+            style={estilos.produto}
           />
           <Text style={estilos.preco}>R$ 99,90</Text>
         </View>
@@ -53,7 +67,7 @@ export default function App() {
           <View style={estilos.topoItem}>
             <Text style={estilos.nome}>Garrafa térmica</Text>
             <View style={estilos.notaBox}>
-              <Text style={estilos.notaTexto}>4.8 ★</Text>
+              <Text style={estilos.notaTexto}>4.8 <Icon name="star" size={12} color="#ffd455" /></Text>
             </View>
           </View>
           <Text style={estilos.descricao}>
@@ -66,8 +80,8 @@ export default function App() {
       <View style={estilos.itemProduto}>
         <View style={estilos.colPreco}>
           <Image
-            source={{ uri: "" }}
-            style={estilos.thumb}
+            source={require('../../assets/img/produto3.png')}
+            style={estilos.produto}
           />
           <Text style={estilos.preco}>R$ 399,90</Text>
         </View>
@@ -75,7 +89,7 @@ export default function App() {
           <View style={estilos.topoItem}>
             <Text style={estilos.nome}>Mochila pop</Text>
             <View style={estilos.notaBox}>
-              <Text style={estilos.notaTexto}>5.0 ★</Text>
+              <Text style={estilos.notaTexto}>5.0 <Icon name="star" size={12} color="#ffd455" /></Text>
             </View>
           </View>
           <Text style={estilos.descricao}>
@@ -88,8 +102,8 @@ export default function App() {
       <View style={estilos.itemProduto}>
         <View style={estilos.colPreco}>
           <Image
-            source={{ uri: "" }}
-            style={estilos.thumb}
+            source={require('../../assets/img/produto4.png')}
+            style={estilos.produto}
           />
           <Text style={estilos.preco}>R$ 99,90</Text>
         </View>
@@ -97,7 +111,7 @@ export default function App() {
           <View style={estilos.topoItem}>
             <Text style={estilos.nome}>Camisa flow</Text>
             <View style={estilos.notaBox}>
-              <Text style={estilos.notaTexto}>4.3 ★</Text>
+              <Text style={estilos.notaTexto}>4.3 <Icon name="star" size={12} color="#ffd455" /></Text>
             </View>
           </View>
           <Text style={estilos.descricao}>
@@ -112,8 +126,13 @@ export default function App() {
           Os melhores produtos esportivos, ofertas exclusivas e tudo o que você precisa
           para turbinar sua performance.
         </Text>
+        <Image
+            source={require('../../assets/img/logo2.png')}
+            style={estilos.imgRodape}
+          />
       </View>
     </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -125,25 +144,26 @@ const estilos = StyleSheet.create({
   conteudoTela: {
     paddingBottom: 40,
   },
-  hero: {
+  Inicio: {
     height: 220,
-    justifyContent: "flex-end",
+    justifyContent: "space-between",
     paddingHorizontal: 18,
     paddingBottom: 18,
     backgroundColor: "#000",
+    width: "110%",
+    overflow: "hidden",
   },
   logo: {
-    color: "#fff",
-    fontWeight: "800",
-    fontSize: 18,
-    position: "absolute",
-    top: 18,
-    left: 18,
+    width: 146,
+    height: 56,
+    marginTop: 9,
+    borderRadius: 6,
+    position: "relative",
   },
-  tituloHero: {
+  tituloInicio: {
     color: "#fff",
-    fontSize: 20,
-    lineHeight: 26,
+    fontSize: 23,
+    lineHeight: 35,
     fontWeight: "600",
   },
   destaque: {
@@ -158,15 +178,16 @@ const estilos = StyleSheet.create({
     paddingHorizontal: 12,
     paddingVertical: 6,
     borderRadius: 6,
+    marginBottom: 9,
   },
   textoFiltro: {
-    color: "#d6cfe6",
+    color: "#ffffffff",
     fontSize: 12,
     fontWeight: "600",
   },
   itemProduto: {
     flexDirection: "row",
-    backgroundColor: "#121212",
+    backgroundColor: "#1f1f1fff",
     borderRadius: 10,
     padding: 12,
     marginBottom: 12,
@@ -175,21 +196,22 @@ const estilos = StyleSheet.create({
   },
   colPreco: {
     width: 92,
-    backgroundColor: "#191919",
+    backgroundColor: "#373737ff",
     borderRadius: 8,
     paddingVertical: 10,
     paddingHorizontal: 6,
     alignItems: "center",
     marginRight: 10,
   },
-  thumb: {
+  produto: {
     width: 56,
     height: 56,
     marginBottom: 8,
     borderRadius: 6,
+    
   },
   preco: {
-    color: "#e6e6e6",
+    color: "#ffffffff",
     fontWeight: "800",
     fontSize: 12,
     textAlign: "center",
@@ -210,7 +232,6 @@ const estilos = StyleSheet.create({
     marginBottom: 6,
   },
   notaBox: {
-    backgroundColor: "#1f1f1f",
     paddingHorizontal: 8,
     paddingVertical: 6,
     borderRadius: 8,
@@ -221,7 +242,7 @@ const estilos = StyleSheet.create({
     fontSize: 12,
   },
   descricao: {
-    color: "#bfbfbf",
+    color: "#e4e4e4ff",
     fontSize: 12,
     lineHeight: 18,
   },
@@ -235,5 +256,8 @@ const estilos = StyleSheet.create({
     color: "#aaa",
     fontSize: 12,
     textAlign: "center",
+  },
+  imgRodape: {
+    marginTop: 20,
   },
 });
