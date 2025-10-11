120237028529dbcc363f72e377afda3726ab360a
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-10 21:05:29 -0300

    correção na navegçaão

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -1,4 +1,4 @@
-import { NavigationProp } from "@react-navigation/native"
+import { NavigationProp, ParamListBase, RouteProp } from "@react-navigation/native"
 import Entrar from "../screens/Entrar";
 import Cadastro from "../screens/Cadastro";
 import Carrinho from "../screens/Carrinho";
@@ -6,20 +6,36 @@ import Inicio from "../screens/Inicio";
 import Pesquisa from "../screens/Pesquisa";
 import Produto from '../screens/Produto';
 import React from "react";
+import { NativeStackNavigationOptions, NativeStackNavigationProp } from "@react-navigation/native-stack";
 
-interface ParamItem<P = NavigationParameter> {
+type NativeStackNavigationOptionsCallback = (props: {
+    route: RouteProp<ParamListBase, "Cadastro">;
+    navigation: NativeStackNavigationProp<ParamListBase, string, undefined>;
+    theme: ReactNavigation.Theme;
+}) => NativeStackNavigationOptions | undefined;
+
+export interface ParamItem<P = NavigationParameter> {
     component: React.ComponentType<P>,
-    params: P | any
+    params: P | any,
+    options?: NativeStackNavigationOptions
+        | NativeStackNavigationOptionsCallback
+        | undefined
 }
 
 class Screens {
-    Entrar: ParamItem = { component: Entrar, params: undefined };
-    Cadastro: ParamItem = { component: Cadastro, params: undefined };
-    Carrinho: ParamItem = { component: Carrinho, params: undefined };
-    Inicio: ParamItem = { component: Inicio, params: undefined };
-    Pesquisa: ParamItem = { component: Pesquisa, params: undefined };
-    Produto: ParamItem = { component: Produto, params: undefined };
+    private constructor() {}
+
+    Entrar: ParamItem = { component: Entrar, params: undefined, options: { headerShown: false } };
+    Cadastro: ParamItem = { component: Cadastro, params: undefined, options: { headerShown: false } };
+    Carrinho: ParamItem = { component: Carrinho, params: undefined, options: { title: "Carrinho" } };
+    Inicio: ParamItem = { component: Inicio, params: undefined, options: { title: "Início" } };
+    Pesquisa: ParamItem = { component: Pesquisa, params: undefined, options: { headerShown: false } };
+    Produto: ParamItem = { component: Produto, params: undefined, options: { headerShown: false } };
+
+    static readonly instance = new Screens();
 }
+
+export const screens = Screens.instance;
 
 export type ScreenNames = keyof Screens;
 
