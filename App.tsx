2be316b47bd637185f90c7f20e7d88f79392397a
guilherme-paiva2007 <guilhermepaiva2007h@gmail.com>
2025-10-10 21:03:00 -0300

    correção na navegação

--- App.tsx
+++ App.tsx
@@ -9,6 +9,7 @@ import Pesquisa from './src/screens/Pesquisa';
 import Produto from './src/screens/Produto';
 import Inicio from './src/screens/Inicio';
 import Carrinho from './src/screens/Carrinho';
+import { ParamItem, screens } from "./src/routes/Routes";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,12 +19,20 @@ export default function App() {
       <StatusBar barStyle="light-content" backgroundColor="rgb(19, 19, 24)" />
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Entrar">
-          <Stack.Screen name="Pesquisa" component={Pesquisa} />
+          {(Object.entries(screens)).map(([name, config]) => (
+            <Stack.Screen
+              key={"Screen." + name}
+              name={name}
+              component={config.component}
+              options={config.options}
+            />
+          ))}
+          {/* <Stack.Screen name="Pesquisa" component={Pesquisa} />
           <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
           <Stack.Screen name="Entrar" component={Entrar} options={{ headerShown: false  }} />
           <Stack.Screen name="Produto" component={Produto} options={{ headerShown: false  }} />
           <Stack.Screen name="Inicio" component={Inicio} options={{ title: "Inicio" }} />
-          <Stack.Screen name="Carrinho" component={Carrinho} options={{ title: "Carrinho" }} />
+          <Stack.Screen name="Carrinho" component={Carrinho} options={{ title: "Carrinho" }} /> */}
         </Stack.Navigator>
       </NavigationContainer>
     </View>
